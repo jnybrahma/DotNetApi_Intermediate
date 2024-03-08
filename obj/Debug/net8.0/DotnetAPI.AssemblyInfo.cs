ba73c79b6576f9df45c9171d532fe760d90a70c0
Brahma, Jnyananjoy <jnybrahma@gmail.com>
2024-03-08 17:15:19 +1000

    Updates

--- obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/DotnetAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13428d4283917574d384cf954211c91bb5f7ff74")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e24c8e6426a4092ff2ef56fe57d483fe9c1a15d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DotnetAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
